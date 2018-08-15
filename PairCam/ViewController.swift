4b83c7dd1fec0bee3b6083183c3a3262e0c5c651
Mark DeLillo <github.med@delillo.org>
2018-08-15 16:50:57 -0400

    Show video from two selectable cams

--- PairCam/ViewController.swift
+++ PairCam/ViewController.swift
@@ -7,13 +7,69 @@
 //
 
 import Cocoa
+import AVFoundation
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var camView1: NSView!
+    @IBOutlet weak var camView2: NSView!
+    @IBOutlet weak var camSelector1: NSPopUpButton!
+    @IBOutlet weak var camSelector2: NSPopUpButton!
+    
+    let session1 = AVCaptureSession()
+    let session2 = AVCaptureSession()
+    var devices: [AVCaptureDevice]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        populateCamNames()
+        startSession(session: session1, view: camView1)
+        startSession(session: session2, view: camView2)
+    }
+    
+    @IBAction func selectCam1(_ sender: Any) {
+        setInput(session: session1, deviceName: camSelector1.selectedItem?.title)
+    }
+    
+    @IBAction func selectCam2(_ sender: Any) {
+        setInput(session: session2, deviceName: camSelector2.selectedItem?.title)
+    }
+    
+    func populateCamNames() {
+        devices = AVCaptureDevice.devices(for: AVMediaType.video)
+        let deviceNames = devices!.map { device in device.localizedName }
+        camSelector1.addItems(withTitles: deviceNames)
+        camSelector2.addItems(withTitles: deviceNames)
+    }
+    
+    func startSession(session:AVCaptureSession, view:NSView) {
+        view.wantsLayer = true
+        session.sessionPreset = AVCaptureSession.Preset.low
+        let previewLayer:AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.frame = view.bounds
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        view.layer?.addSublayer(previewLayer)
+        setInput(session: session)
+        session.startRunning()
+    }
+    
+    func setInput(session:AVCaptureSession, deviceName:String? = "") {
+        var device:AVCaptureDevice
+        if deviceName == "" {
+            device = AVCaptureDevice.default(for: AVMediaType.video)!
+        } else {
+            device = devices!.first { (d) -> Bool in
+                d.localizedName == deviceName
+            }!
+        }
+        let deviceInput : AVCaptureDeviceInput = try! AVCaptureDeviceInput(device: device)
+        
+        for existingInput in session.inputs {
+            session.removeInput(existingInput)
+        }
+        if session.canAddInput(deviceInput) {
+            session.addInput(deviceInput)
+        }
     }
 
     override var representedObject: Any? {
